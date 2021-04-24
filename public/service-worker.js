5449107409f717509e5cbf9aa1f550a158bcc4d5
Tristan LaRoche <laroche11235@gmail.com>
2021-04-24 15:17:52 -0400

    changes to manifest and sw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,12 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/style.css",
+    "/styles.css",
     "/manifest.webmanifest",
-    "/index.js"
+    "/index.js",
     // "/favicon.ico",
-    // "/assets/images/icons/icon-72x72.png",
-    // "/assets/images/icons/icon-96x96.png",
-    // "/assets/images/icons/icon-128x128.png",
-    // "/assets/images/icons/icon-144x144.png",
-    // "/assets/images/icons/icon-152x152.png",
-    // "/assets/images/icons/icon-192x192.png",
-    // "/assets/images/icons/icon-384x384.png",
-    // "/assets/images/icons/icon-512x512.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
   ];
   
   const CACHE_NAME = "static-cache-v2";
